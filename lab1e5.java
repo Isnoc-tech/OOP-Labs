a42a0a07f3975371596f9fbbef19947d94d5ba3e
Thusala Piyarisi <49372490+Isnoc-tech@users.noreply.github.com>
2023-07-22 23:50:21 +0530

    Create lab1e5.java

---
+++ lab1e5.java
@@ -0,0 +1,42 @@
+public class TrianglePrint {
+
+    public static void main(String[] args) {
+        int size = 5; // Change this value to adjust the size of the triangle
+        printTriangle(size);
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            // Align Center the stars
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars for each row
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
+
+
+public class SquarePrint {
+
+    public static void main(String[] args) {
+        int size = 5; 
+        printSquare(size);
+    }
+
+    public static void printSquare(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
