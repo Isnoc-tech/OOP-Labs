d4309d7d75444bd83cc0f6a05a23eb55038477a0
Thusala Piyarisi <49372490+Isnoc-tech@users.noreply.github.com>
2023-08-07 10:56:40 +0530

    Create lab2e1.java

---
+++ lab2e1.java
@@ -0,0 +1,18 @@
+public class ToKilometerConvertor {
+
+    public static void main(String[] args) {
+
+        int miles, yards;
+        double kilometers;
+
+        miles = 26;
+        yards = 385;
+
+        double kilometersPerMile = 1.609;
+        double yardsPerMile = 1760;
+        double totalMiles = miles + (yards / yardsPerMile);
+        kilometers = totalMiles * kilometersPerMile;
+
+        System.out.println("The marathon distance in kilometers: " + kilometers);
+    }
+}
